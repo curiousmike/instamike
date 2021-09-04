81bd0568cc34e84d3b38a1c6379356767963a1f2
Michael Coustier <curiousmike@gmail.com>
2021-09-03 17:17:41 -0700

    get basics of users post grid working

--- src/components/userProfileView/userPostGrid/index.js
+++ src/components/userProfileView/userPostGrid/index.js
@@ -1,13 +1,23 @@
-import { Container, ImageContainer } from './styles'
+import { Container, MainImageContainer, ImageContainer } from './styles'
+import './userPostGrid.css';
+
 function UserPostGrid({posts}) {
 	return (
 		<Container>
-		{
-			posts.map( (post, index)=>(
-			<ImageContainer>
-				<img src={post.image} alt={'the alt'} key={index} width={'100%'} height={'undefined'} aspectRatio={1}/>
-			</ImageContainer>
-		))}
+			<MainImageContainer>
+			{
+				posts.map( (post, index)=>(
+				<ImageContainer>
+					<img className = "ImgClass"
+						src={post.image}
+						alt={'the alt'}
+						key={index}
+						width={'100%'}
+						height={'100%'}
+						/>
+				</ImageContainer>
+			))}
+		</MainImageContainer>
 		</Container>
 	)
 }
--- src/components/userProfileView/userPostGrid/styles.js
+++ src/components/userProfileView/userPostGrid/styles.js
@@ -1,13 +1,22 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    height: 80vh;
+`;
+
+export const MainImageContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
+    align-content: start;
     width: 100%;
     height: 100%;
+    overflow-y: scroll;
+    scrollbar-width: thin;
 `;
 
 export const ImageContainer = styled.div`
-    width: 25%;
-    padding: 8px;
+    width: 33%;
+    height: 25%;
+    padding: 16px;
 `;
+
