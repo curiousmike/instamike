d469e7fc8525936160f66b9ba9f6b063f0d06fbc
Michael Coustier <curiousmike@gmail.com>
2021-09-18 22:38:48 -0700

    broke- add ability to stop following a user

--- src/components/userProfileView/followView/index.js
+++ src/components/userProfileView/followView/index.js
@@ -1,8 +1,12 @@
-import { Container} from './styles'
+import {useState} from 'react';
+import { Container, FollowTitle} from './styles'
 import {List, ListItem, ListItemText, ListItemAvatar, 
-    Avatar, IconButton, ListItemSecondaryAction } from '@material-ui/core';
+    Avatar, IconButton, ListItemSecondaryAction,Tooltip, Menu, MenuItem } from '@material-ui/core';
+
 import IconVert from '@material-ui/icons/MoreVert';
+
 function FollowView({usersData, user, onSelectUser, followers}) {
+    const [anchorEl, setAnchorEl] = useState(null);
     const dense = false;
     const secondary = false;
     let users;
@@ -18,28 +22,51 @@ function FollowView({usersData, user, onSelectUser, followers}) {
         });
     }
 
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    }
+
+    const stopFollowing = (index, user) => {
+        alert('broke - also last user passed - closure issue? ' + user.name);
+        handleMenuClose();
+    }
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
 	return (
-		<Container>
-            { followers ? 'Follwers' : 'Following'}
-            <List dense={dense} style={{width: '100%'}}>
+        <Container>
+            <FollowTitle>{ followers ? 'Followers' : 'Following'}</FollowTitle>
+            <List dense = {dense} style = {{width: '100%'}}>
                 { users.map( (user, index)=>(
-                    <ListItem onClick={()=>onSelectUser(user)} key={index}>
+                    <ListItem onClick = {()=>onSelectUser(user)} key = {index}>
                         <ListItemAvatar>
-						<Avatar alt={user.name} src={user.avatar}/>
+                            <Avatar alt = {user.name} src = {user.avatar}/>
                         </ListItemAvatar>
                         <ListItemText
-                        primary={user?.name}
-                        secondary={secondary ? 'Secondary text' : null}
+                            primary={user?.name}
+                            secondary={secondary ? 'Secondary text' : null}
                         />
                         <ListItemSecondaryAction>
-                        <IconButton edge="end" aria-label="delete">
-                            <IconVert />
-                        </IconButton>
+                            <IconButton aria-label="home" onClick = {(e)=>handleMenuOpen(e)}>
+                                <IconVert />
+                            </IconButton>
+                            <Menu
+                                id={id}
+                                open={open}
+                                anchorEl={anchorEl}
+                                onClose={handleMenuClose}
+                            >
+                                <MenuItem onClick={()=>stopFollowing(index, user)}>Stop following {user.name}</MenuItem>
+                            </Menu>
                         </ListItemSecondaryAction>
                     </ListItem>
                 ))}
             </List>
-          </Container>
+        </Container>
 	)
 }
 
--- src/components/userProfileView/followView/styles.js
+++ src/components/userProfileView/followView/styles.js
@@ -3,4 +3,10 @@ import styled from 'styled-components'
 export const Container = styled.div`
     display: flex;
     height: 80vh;
+    flex-direction: column;
 `;
+
+export const FollowTitle = styled.div`
+    margin-left: 8px;
+    font-size: 24px;
+`
