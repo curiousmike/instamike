430ec6900926f78bfd12a385272a789194b262bc
Michael Coustier <curiousmike@gmail.com>
2021-09-25 21:03:45 -0700

    make it so EditProfile is only available to YouUser

--- src/components/userProfileView/header/index.js
+++ src/components/userProfileView/header/index.js
@@ -1,47 +1,91 @@
-import { Container, TopContainer, ItemContainer, RightContainer, NameContainer, AvatarContainer,
-    RowContainer, DescriptionContainer, BottomContainer, NameAndAvatarContainer, EditProfileButton } from './styles'
-import HeaderItem from '../headerItem';
-import { Avatar, Button } from '@material-ui/core';
+import { useContext } from "react";
+import { StoreContext } from "../../../store";
+import {
+  Container,
+  TopContainer,
+  ItemContainer,
+  RightContainer,
+  NameContainer,
+  AvatarContainer,
+  RowContainer,
+  DescriptionContainer,
+  BottomContainer,
+  NameAndAvatarContainer,
+  EditProfileButton,
+} from "./styles";
+import HeaderItem from "../headerItem";
+import { Avatar, Button } from "@material-ui/core";
 
-function UserProfileHeader({usersData, user, onSelectPosts, onSelectFollowers, onSelectFollowing, editProfile}) {
-    const userData = usersData.filter(obj=>{ return obj.name === user.name})[0];
-	return (
-        <Container>
-    		<TopContainer>
-                <NameAndAvatarContainer>
-                    <NameContainer>
-                        {userData.name}
-                    </NameContainer>
-                    <AvatarContainer>
-                        <Avatar alt={userData.name} src={userData.avatar} style={{width: '10vh', height: '10vh'}}/>
-                    </AvatarContainer>
-                </NameAndAvatarContainer>
-                <RightContainer>
-                    <RowContainer>
-                        <ItemContainer onClick={onSelectPosts}>
-                            <HeaderItem number={userData.posts.length ? userData.posts.length : '0'} label={'Posts'} />
-                        </ItemContainer>
-                        <ItemContainer onClick={onSelectFollowers}>
-                            <HeaderItem number={userData.followers.length} label={'Followers'} />
-                        </ItemContainer>
-                        <ItemContainer onClick={onSelectFollowing}>
-                            <HeaderItem number={userData.following.length} label={'Following'} />
-                        </ItemContainer>
-                    </RowContainer>                
-                    <EditProfileButton>
-                        <Button variant = "outlined" size="small" onClick={()=>editProfile()}> Edit your profile</Button>
-                    </EditProfileButton>
-                </RightContainer>
-            </TopContainer>
-            <BottomContainer>
-                <RowContainer>
-                    <DescriptionContainer>
-                        {userData.description ? userData.description : 'No description'}
-                    </DescriptionContainer>
-                </RowContainer>
-            </BottomContainer>
-        </Container>
-	)
+function UserProfileHeader({
+  usersData,
+  user,
+  onSelectPosts,
+  onSelectFollowers,
+  onSelectFollowing,
+  editProfile,
+}) {
+  const myContext = useContext(StoreContext);
+  const userData = usersData.filter((obj) => {
+    return obj.name === user.name;
+  })[0];
+  const showEditProfileButton = user.name === myContext.youUser.name;
+  return (
+    <Container>
+      <TopContainer>
+        <NameAndAvatarContainer>
+          <NameContainer>{userData.name}</NameContainer>
+          <AvatarContainer>
+            <Avatar
+              alt={userData.name}
+              src={userData.avatar}
+              style={{ width: "10vh", height: "10vh" }}
+            />
+          </AvatarContainer>
+        </NameAndAvatarContainer>
+        <RightContainer>
+          <RowContainer>
+            <ItemContainer onClick={onSelectPosts}>
+              <HeaderItem
+                number={userData.posts.length ? userData.posts.length : "0"}
+                label={"Posts"}
+              />
+            </ItemContainer>
+            <ItemContainer onClick={onSelectFollowers}>
+              <HeaderItem
+                number={userData.followers.length}
+                label={"Followers"}
+              />
+            </ItemContainer>
+            <ItemContainer onClick={onSelectFollowing}>
+              <HeaderItem
+                number={userData.following.length}
+                label={"Following"}
+              />
+            </ItemContainer>
+          </RowContainer>
+          {showEditProfileButton && (
+            <EditProfileButton>
+              <Button
+                variant="outlined"
+                size="small"
+                onClick={() => editProfile()}
+              >
+                {" "}
+                Edit your profile
+              </Button>
+            </EditProfileButton>
+          )}
+        </RightContainer>
+      </TopContainer>
+      <BottomContainer>
+        <RowContainer>
+          <DescriptionContainer>
+            {userData.description ? userData.description : "No description"}
+          </DescriptionContainer>
+        </RowContainer>
+      </BottomContainer>
+    </Container>
+  );
 }
 
 export default UserProfileHeader;
