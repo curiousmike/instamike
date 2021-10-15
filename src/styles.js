0521bd486c6aafc86ca05b44aa5c1d37acba2c98
Michael Coustier <curiousmike@gmail.com>
2021-10-14 17:51:57 -0700

    add missing file

---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const InnerContent = styled.main`
+  height: 80vh;
+  margin: 2px 0px 2px 0px;
+`;
+export const LoadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+`;
+
+export const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+  font-size: 24px;
+  font-weight: 700;
+  color: red;
+`;
