b2d4d711157567ef7ec9788af7e5406583bc431b
Michael Coustier <curiousmike@gmail.com>
2021-11-28 22:25:16 -0800

    trying to improve small image display

--- src/components/singlePost/components/singlePostImage/index.js
+++ src/components/singlePost/components/singlePostImage/index.js
@@ -1,12 +1,29 @@
-import { Container, ImageContainer } from './styles'
+import { useState } from "react";
+
+import { Container, ImageContainer } from "./styles";
 
 function SinglePostImage({ imgSrc, onImageClick, isProfile }) {
+  const [aspectRatio, setAspectRatio] = useState("horiz");
+  const onImageLoad = (img) => {
+    if (img.target.offsetHeight > img.target.offsetWidth) {
+      setAspectRatio("vert");
+    } else {
+      setAspectRatio("horiz");
+    }
+  };
+  const heightToUse = isProfile ? "63vh" : "78vh";
   return (
     <Container>
       <ImageContainer>
         <img
           alt="my alt"
-          style={{ maxHeight: isProfile ? "65vh" : "80vh", maxWidth: "100%" }}
+          onLoad={onImageLoad}
+          style={{
+            maxHeight: heightToUse,
+            minHeight: aspectRatio === "vert" ? heightToUse : "",
+            maxWidth: "90%",
+            minWidth: aspectRatio === "horiz" ? "60vw" : "1vw",
+          }}
           src={imgSrc}
           onClick={onImageClick}
         ></img>
