d0a5bf7567b00dbf12682a130a218e173d2ac9e4
Michael Coustier <curiousmike@gmail.com>
2022-01-02 19:26:07 -0800

    fixing image aspect ratio / size

--- src/components/singlePost/components/singlePostImage/index.js
+++ src/components/singlePost/components/singlePostImage/index.js
@@ -1,14 +1,29 @@
 import { Container, ImageContainer } from './styles'
+import {useState} from 'react';
 
 function SinglePostImage({ imgSrc, onImageClick, isProfile }) {
+  const [isWide, setIsWide] = useState(false);
+  const maxHeight = isProfile ? '60vh' : '80vh';
+  const handleLoad = ({target}) => {
+    if (target.offsetWidth >= target.offsetHeight) {
+      setIsWide(true);
+    } else {
+      setIsWide(false);
+    }
+  }
+
   return (
     <Container>
       <ImageContainer>
         <img
           alt="my alt"
-          style={{ maxHeight: isProfile ? "65vh" : "80vh", maxWidth: "100%" }}
-          src={imgSrc}
-          onClick={onImageClick}
+          style={{
+            width: isWide ? '90%' : '',
+            height: isWide ? '' : maxHeight,
+          }}
+          src = {imgSrc}
+          onClick = {onImageClick}
+          onLoad = {(e)=>handleLoad(e)}
         ></img>
       </ImageContainer>
     </Container>
